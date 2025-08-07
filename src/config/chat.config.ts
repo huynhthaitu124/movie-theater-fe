2508623b0dcf2e8ca78ec7a375e4e6fdbe5c4c50
huynh tu <huynhthaitu124@gmail.com>
2025-08-07 10:38:01 +0700

    modify

--- src/config/chat.config.ts
+++ src/config/chat.config.ts
@@ -1,6 +1,6 @@
 // SignalR Configuration
 export const SIGNALR_CONFIG = {
-  HUB_URL: 'https://movie-theater-hfeafxgheubvdwfr.southeastasia-01.azurewebsites.net/chathub', // SignalR Hub URL từ backend
+  HUB_URL: 'https://movietheater-api.calmbeach-b063071e.eastasia.azurecontainerapps.io/chathub', // SignalR Hub URL từ backend
   RECONNECT_INTERVALS: [0, 2000, 10000, 30000], // Reconnection intervals in milliseconds
   MAX_RECONNECT_ATTEMPTS: 5
 };
